a03503bebe0e8a1ea4ac1d05ea9007df3ede89e2
Chobogang <96684541+Chobogang@users.noreply.github.com>
2022-07-27 01:09:18 +0000

    greaterId = small number > big number

---
+++ main.py
@@ -0,0 +1,24 @@
+n, m = map(int,input().split())
+score =list(map(int,input().split()))
+max = 0
+greaterId = 0
+
+for i in range(m) :
+    id = input().split()
+    count = 0 
+    for k in range(1, 5) :
+        if id[k] == 'O' :
+            count += score[k-1]
+        if count >= max :
+            if int(id[0]) >= greaterId :
+                greaterId = int(id[0])
+                max = count
+                print('----------------')
+                print(max)
+                print(greaterId)
+                print('----------------')
+            else : continue
+
+print(greaterId, max)
+            
+    
