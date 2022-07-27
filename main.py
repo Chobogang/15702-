36909306bb74b0684ed1b5991bb5d0261648e463
Chobogang <96684541+Chobogang@users.noreply.github.com>
2022-07-27 02:03:42 +0000

    문제 개수 범위(최종)

--- main.py
+++ main.py
@@ -2,23 +2,21 @@
 n, m = map(int, input().split())
 score = list(map(int, input().split()))
 max = 0
-greaterId = 0
+greaterId = 100001
 
 for i in range(m):
     id = input().split()
     count = 0
-    for k in range(1, 5):
+    for k in range(1, n + 1):
         if id[k] == 'O':
             count += score[k - 1]
     if count > max:
         greaterId = int(id[0])
         max = count
     elif count == max:
-        if int(id[0]) < greaterId:
-            count = max
-            greaterId = int(id[0])          
-        else:
-            continue
-    else : continue
+        count = max
+        greaterId = min(int(id[0]), greaterId)
+    else:
+        continue
 
 print(greaterId, max)
