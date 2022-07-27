f486c095974bc7ddb2d6f8ea61546d85e497f025
Chobogang <96684541+Chobogang@users.noreply.github.com>
2022-07-27 01:36:29 +0000

    max.equals(count)

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-n, m = map(int,input().split())
-score =list(map(int,input().split()))
+# 문제 수 n, 학생 수 m
+n, m = map(int, input().split())
+score = list(map(int, input().split()))
 max = 0
 greaterId = 0
 
-for i in range(m) :
+for i in range(m):
     id = input().split()
-    count = 0 
-    for k in range(1, 5) :
-        if id[k] == 'O' :
-            count += score[k-1]
-    if count >= max :
-        if int(id[0]) <= greaterId :
-            greaterId = int(id[0])
-            max = count
-        else : 
-            greaterId = int(id[0])
-            max = count
-            
+    count = 0
+    for k in range(1, 5):
+        if id[k] == 'O':
+            count += score[k - 1]
+    if count > max:
+        greaterId = int(id[0])
+        max = count
+    elif count == max:
+        if int(id[0]) < greaterId:
+            count = max
+            greaterId = int(id[0])          
+        else:
+            continue
+    else : continue
+
 print(greaterId, max)
-            
-    
